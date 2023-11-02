ae0cda868d837d63a6ba031f5cce1c1e9c736ac1
ScoRoss <rosslam5@live.com>
2023-11-02 12:36:50 +0000

    changed folder structure and got it working
    make changes as above

--- babylonProj/element02/src/createStartScene.ts
+++ babylonProj/element02/src/createStartScene.ts
@@ -1,50 +1,172 @@
+//-----------------------------------------------------
+//TOP OF CODE - IMPORTING BABYLONJS
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import {
     Scene,
     ArcRotateCamera,
     Vector3,
+    Vector4,
     HemisphericLight,
+    SpotLight,
     MeshBuilder,
     Mesh,
     Light,
     Camera,
     Engine,
+    StandardMaterial,
+    Texture,
+    Color3,
+    Space,
+    ShadowGenerator,
+    PointLight,
+    DirectionalLight,
+    CubeTexture,
+    Sprite,
+    SpriteManager,
   } from "@babylonjs/core";
+  //----------------------------------------------------
   
-  
+  //----------------------------------------------------
+  //MIDDLE OF CODE - FUNCTIONS
+  //Create Terrain
+  function createTerrain(scene: Scene) {
+    //Create large ground for valley environment
+    const largeGroundMat = new StandardMaterial("largeGroundMat");
+    largeGroundMat.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/valleygrass.png");
+    
+    const largeGround = MeshBuilder.CreateGroundFromHeightMap("largeGround", "https://assets.babylonjs.com/environments/villageheightmap.png", {width:150, height:150, subdivisions: 20, minHeight:0, maxHeight: 10});
+    largeGround.material = largeGroundMat;
+    return largeGround;
+  }
+
+  //Create more detailed ground
+  function createGround(scene: Scene) {
+    //Create Village ground
+    const groundMat = new StandardMaterial("groundMat");
+    groundMat.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/villagegreen.png");
+    groundMat.diffuseTexture.hasAlpha = true;
+
+    const ground = MeshBuilder.CreateGround("ground", {width:24, height:24});
+    ground.material = groundMat;
+    ground.position.y = 0.01;
+    return ground;
+  }
+
+  //Create Skybox
+  function createSkybox(scene: Scene) {
+    //Skybox
+    const skybox = MeshBuilder.CreateBox("skyBox", {size:150}, scene);
+	  const skyboxMaterial = new StandardMaterial("skyBox", scene);
+	  skyboxMaterial.backFaceCulling = false;
+	  skyboxMaterial.reflectionTexture = new CubeTexture("textures/skybox", scene);
+	  skyboxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE;
+	  skyboxMaterial.diffuseColor = new Color3(0, 0, 0);
+	  skyboxMaterial.specularColor = new Color3(0, 0, 0);
+	  skybox.material = skyboxMaterial;
+    return skybox;
+  }
+
+  //Creating sprite trees
+  function createTrees(scene: Scene) {
+    const spriteManagerTrees = new SpriteManager("treesManager", "textures/palmtree.png", 2000, {width: 512, height: 1024}, scene);
+
+    //We create trees at random positions
+    for (let i = 0; i < 500; i++) {
+        const tree = new Sprite("tree", spriteManagerTrees);
+        tree.position.x = Math.random() * (-30);
+        tree.position.z = Math.random() * 20 + 8;
+        tree.position.y = 0.5;
+    }
+
+    for (let i = 0; i < 500; i++) {
+        const tree = new Sprite("tree", spriteManagerTrees);
+        tree.position.x = Math.random() * (25) + 7;
+        tree.position.z = Math.random() * -35  + 8;
+        tree.position.y = 0.5;
+    }
+    return spriteManagerTrees;
+  }
+
   function createBox(scene: Scene) {
-    let box = MeshBuilder.CreateBox("box",{size: 7}, scene);
-    box.position.y = 7;
+    const boxMat = new StandardMaterial("boxMat");
+    boxMat.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/cubehouse.png");
+
+    //options parameter to set different images on each side
+    const faceUV: Vector4[] = [];
+    faceUV[0] = new Vector4(0.5, 0.0, 0.75, 1.0); //rear face
+    faceUV[1] = new Vector4(0.0, 0.0, 0.25, 1.0); //front face
+    faceUV[2] = new Vector4(0.25, 0, 0.5, 1.0); //right side
+    faceUV[3] = new Vector4(0.75, 0, 1.0, 1.0); //left side
+    // top 4 and bottom 5 not seen so not set
+
+    const box = MeshBuilder.CreateBox("box", {faceUV: faceUV, wrap: true});
+    box.position.y = 0.5;
+    box.material = boxMat;
     return box;
   }
 
-  
-  function createLight(scene: Scene) {
+  function createRoof(scene: Scene) {
+    const roofMat = new StandardMaterial("roofMat");
+    roofMat.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/roof.jpg");
+
+    const roof = MeshBuilder.CreateCylinder("roof", {diameter: 1.3, height: 1.2, tessellation: 3});
+    roof.scaling.x = 0.75;
+    roof.rotation.z = Math.PI / 2;
+    roof.position.y = 1.22;
+    roof.material = roofMat;
+    return roof;
+  }
+
+  function createHouse(scene: Scene) {
+    const box = createBox(scene);
+    const roof = createRoof(scene);
+    const house = Mesh.MergeMeshes([box, roof]);
+    return house;
+  }
+
+  //----------------------------------------------------------------------------------------------
+
+  function createAnyLight(scene: Scene, index: number, px: number, py: number, pz: number, colX: number, colY: number, colZ: number, mesh: Mesh) {
+    // only spotlight, point and directional can cast shadows in BabylonJS
+    switch (index) {
+      case 1: //hemispheric light
+        const hemiLight = new HemisphericLight("hemiLight", new Vector3(px, py, pz), scene);
+        hemiLight.intensity = 0.1;
+        return hemiLight;
+        break;
+      case 2: //spot light
+        const spotLight = new SpotLight("spotLight", new Vector3(px, py, pz), new Vector3(0, -1, 0), Math.PI / 3, 10, scene);
+        spotLight.diffuse = new Color3(colX, colY, colZ); //0.39, 0.44, 0.91
+        let shadowGenerator = new ShadowGenerator(1024, spotLight);
+        shadowGenerator.addShadowCaster(mesh);
+        shadowGenerator.useExponentialShadowMap = true;
+        return spotLight;
+        break;
+      case 3: //point light
+        const pointLight = new PointLight("pointLight", new Vector3(px, py, pz), scene);
+        pointLight.diffuse = new Color3(colX, colY, colZ); //0.39, 0.44, 0.91
+        shadowGenerator = new ShadowGenerator(1024, pointLight);
+        shadowGenerator.addShadowCaster(mesh);
+        shadowGenerator.useExponentialShadowMap = true;
+        return pointLight;
+        break;
+    }
+  }
+ 
+  function createHemiLight(scene: Scene) {
     const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-    light.intensity = 0.7;
+    light.intensity = 0.8;
     return light;
   }
-  
-  
-  function createSphere(scene: Scene) {
-    let sphere = MeshBuilder.CreateSphere(
-      "sphere",
-      { diameter: 2, segments: 32 },
-      scene,
-    );
-    sphere.position.y = 1;
-    return sphere;
-  }
-  
-  function createGround(scene: Scene) {
-    let ground = MeshBuilder.CreateGround(
-      "ground",
-      { width: 6, height: 6 },
-      scene,
-    );
-    return ground;
-  }
+
+  //PREVIOUS METHODS
+  // function createSpotLight(scene: Scene, px: number, py: number, pz: number) {
+  //   var light = new SpotLight("spotLight", new Vector3(-1, 1, -1), new Vector3(0, -1, 0), Math.PI / 2, 10, scene);
+  //   light.diffuse = new Color3(0.39, 0.44, 0.91);
+	//   light.specular = new Color3(0.22, 0.31, 0.79);
+  //   return light;
+  // }
   
   function createArcRotateCamera(scene: Scene) {
     let camAlpha = -Math.PI / 2,
@@ -62,24 +184,43 @@ import {
     camera.attachControl(true);
     return camera;
   }
+  //----------------------------------------------------------
   
+  //----------------------------------------------------------
+  //BOTTOM OF CODE - MAIN RENDERING AREA FOR YOUR SCENE
   export default function createStartScene(engine: Engine) {
     interface SceneData {
       scene: Scene;
-      box?: Mesh;
-      light?: Light;
-      sphere?: Mesh;
+      terrain?: Mesh;
       ground?: Mesh;
+      skybox?: Mesh;
+      trees?: SpriteManager;
+      box?: Mesh;
+      roof?: Mesh;
+      house?: Mesh;
+      light?: Light;
+      hemisphericLight?: HemisphericLight;
       camera?: Camera;
     }
   
     let that: SceneData = { scene: new Scene(engine) };
     that.scene.debugLayer.show();
-  
-    that.box = createBox(that.scene);
-    that.light = createLight(that.scene);
-    that.sphere = createSphere(that.scene);
+
+    //any further code goes here
+    that.terrain = createTerrain(that.scene);
     that.ground = createGround(that.scene);
+    that.skybox = createSkybox(that.scene);
+    that.trees = createTrees(that.scene);
+
+    //housing
+    that.box = createBox(that.scene);
+    that.roof = createRoof(that.scene);
+    //that.house = createHouse(that.scene);
+    //const house = Mesh.MergeMeshes([that.box, that.roof], true, false, undefined, false, true);
+  
+    //Scene Lighting & Camera
+    that.hemisphericLight = createHemiLight(that.scene);
     that.camera = createArcRotateCamera(that.scene);
     return that;
   }
+  //----------------------------------------------------

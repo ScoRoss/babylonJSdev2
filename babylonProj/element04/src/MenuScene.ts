ed269ed62c165469f71519de28065ef33e4c3e56
ScoRoss <rosslam5@live.com>
2023-11-23 10:50:24 +0000

    building for html
    making builds for html js imports

--- babylonProj/element04/src/MenuScene.ts
+++ babylonProj/element04/src/MenuScene.ts
@@ -1,5 +1,6 @@
 //----------------------Ross-Lamont-------------------------------
 //TOP OF CODE - IMPORTING BABYLONJS
+import setSceneIndex from "./index";
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import {
@@ -52,131 +53,12 @@ import {
     button.onPointerUpObservable.add(function() {
     console.log("THE BUTTON HAS BEEN CLICKED");
     buttonClick.play();
+    setSceneIndex(1);
     });
     advtex.addControl(button);
     return button;
     }
 
-  //----------------------------------------------------
-  //initialisation of physics havok
-  // let initializedHavok;
-
-  // HavokPhysics().then((havok) => {
-  //   initializedHavok = havok;
-  // });
-  
-  // const havokInstance = await HavokPhysics();
-  // const havokPlugin = new HavokPlugin(true, havokInstance);
-  // globalThis.HK = await HavokPhysics();
-  // //-----------------------------------------------
-
-    
-  // globalThis.HK = await HavokPhysics();
-  // //MIDDLE OF CODE - FUNCTIONS
-  // let keyDownMap: any[] = [];
-
-  // function importPlayerMesh(scene: Scene, collider: Mesh, x: number, y: number) {
-  //   let tempItem = { flag: false } 
-  //   let item: any = SceneLoader.ImportMesh("", "./models/", "dummy3.babylon", scene, function(newMeshes, particleSystems, skeletons) {
-  //     let mesh = newMeshes[0];
-  //     let skeleton = skeletons[0];
-  //     skeleton.animationPropertiesOverride = new AnimationPropertiesOverride();
-  //     skeleton.animationPropertiesOverride.enableBlending = true;
-  //     skeleton.animationPropertiesOverride.blendingSpeed = 0.05;
-  //     skeleton.animationPropertiesOverride.loopMode = 1; 
-
-  //     let walkRange: any = skeleton.getAnimationRange("YBot_Walk");
-  //     // let runRange: any = skeleton.getAnimationRange("YBot_Run");
-  //     // let leftRange: any = skeleton.getAnimationRange("YBot_LeftStrafeWalk");
-  //     // let rightRange: any = skeleton.getAnimationRange("YBot_RightStrafeWalk");
-  //     // let idleRange: any = skeleton.getAnimationRange("YBot_Idle");
-
-  //     let animating: boolean = false;
-
-  //     scene.onBeforeRenderObservable.add(()=> {
-  //       let keydown: boolean = false;
-  //       if (keyDownMap["w"] || keyDownMap["ArrowUp"]) {
-  //         mesh.position.z += 0.1;
-  //         mesh.rotation.y = 0;
-  //         keydown = true;
-  //       }
-  //       if (keyDownMap["a"] || keyDownMap["ArrowLeft"]) {
-  //         mesh.position.x -= 0.1;
-  //         mesh.rotation.y = 3 * Math.PI / 2;
-  //         keydown = true;
-  //       }
-  //       if (keyDownMap["s"] || keyDownMap["ArrowDown"]) {
-  //         mesh.position.z -= 0.1;
-  //         mesh.rotation.y = 2 * Math.PI / 2;
-  //         keydown = true;
-  //       }
-  //       if (keyDownMap["d"] || keyDownMap["ArrowRight"]) {
-  //         mesh.position.x += 0.1;
-  //         mesh.rotation.y = Math.PI / 2;
-  //         keydown = true;
-  //       }
-
-  //       if (keydown) {
-  //         if (!animating) {
-  //           animating = true;
-  //           scene.beginAnimation(skeleton, walkRange.from, walkRange.to, true);
-  //         }
-  //       } else {
-  //         animating = false;
-  //         scene.stopAnimation(skeleton);
-  //       } 
-  //       //collision 
-  //       if (mesh.intersectsMesh(collider)){
-  //         console.log("COLLIDED");
-  //       }
-
-  //     });
-  //     // physicis colision 
-  //     item = mesh;
-  //     let playerAggregate = new PhysicsAggregate(item, PhysicsShapeType.CAPSULE,{mass: 0},scene);
-  //     playerAggregate.body.disablePreStep = false;
-  //   });
-  //   return item;
-  // }
-
-  // function actionManager(scene: Scene){
-  //   scene.actionManager = new ActionManager(scene);
-
-  //   scene.actionManager.registerAction(
-  //     new ExecuteCodeAction(
-  //       {
-  //         trigger: ActionManager.OnKeyDownTrigger,
-  //         //parameters: 'w'
-  //       },
-  //       function(evt) {keyDownMap[evt.sourceEvent.key] = true; }
-  //     )
-  //   );
-  //   scene.actionManager.registerAction(
-  //     new ExecuteCodeAction(
-  //       {
-  //         trigger: ActionManager.OnKeyUpTrigger
-  //       },
-  //       function(evt) {keyDownMap[evt.sourceEvent.key] = false; }
-  //     )
-  //   );
-  //   return scene.actionManager;
-  // } 
-
-  // //Create more detailed ground
-  // function createBox(scene, x: number, y: number, z: number){
-  //   let box: Mesh = MeshBuilder.CreateBox("box", scene);
-  //   box.position.x = x;
-  //   box.position.y = y;
-  //   box.position.z = z;
-  //   const boxAggregate = new PhysicsAggregate(box, PhysicsShapeType.BOX, { mass: 1 }, scene);
-  //   return box;
-  // }
-    
-  // function createGround(scene: Scene) {
-  //   const ground: Mesh = MeshBuilder.CreateGround("ground", {height: 10, width: 10, subdivisions: 4});
-  //   const groundAggregate = new PhysicsAggregate(ground, PhysicsShapeType.BOX, { mass: 0 }, scene);
-  //   return ground;
-  // }
 
   //----------------------------------------------------------------------------------------------
   //Create Skybox
@@ -228,13 +110,7 @@ import {
     return light;
   }
 
-  //PREVIOUS METHODS
-  // function createSpotLight(scene: Scene, px: number, py: number, pz: number) {
-  //   var light = new SpotLight("spotLight", new Vector3(-1, 1, -1), new Vector3(0, -1, 0), Math.PI / 2, 10, scene);
-  //   light.diffuse = new Color3(0.39, 0.44, 0.91);
-	//   light.specular = new Color3(0.22, 0.31, 0.79);
-  //   return light;
-  // }
+
   
   function createArcRotateCamera(scene: Scene) {
     let camAlpha = -Math.PI / 2,
@@ -256,7 +132,7 @@ import {
   
   //----------------------------------------------------------
   //BOTTOM OF CODE - MAIN RENDERING AREA FOR YOUR SCENE
-  export default function createStartScene(engine: Engine) {
+  export default function MenuScene(engine: Engine) {
     interface SceneData {
       scene: Scene;
       box?: Mesh;
@@ -282,10 +158,7 @@ import {
     let advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("myUI",true);
     let button1 = createSceneButton(that.scene, "but1", "START GAME", "0px", "-75px", advancedTexture);
     //-------------------------------------
-    // that.ground = createGround(that.scene);
-    // that.box = createBox(that.scene, 2, 2, 2);
-    // that.importMesh = importPlayerMesh(that.scene, that.box, 0, 0);
-    // that.actionManager = actionManager(that.scene);
+
     that.skybox = createSkybox(that.scene);
     //Scene Lighting & Camera
     that.hemisphericLight = createHemiLight(that.scene);
